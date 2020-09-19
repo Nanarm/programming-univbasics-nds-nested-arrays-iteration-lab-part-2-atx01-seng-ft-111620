92367357b52c3183da3a2c3a88de5c499adadc57
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 22:43:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,12 +7,9 @@ while count < array_of_arrays.length do
   inner_count = 0
   while inner_count < array_of_arrays[count].length do
     results_array = array_of_arrays.map {|a| a.min}
-     
     inner_count += 1
   end
- 
   count += 1
 end
- 
 results_array
 end
