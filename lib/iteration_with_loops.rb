de07b481da24974c0bd2cf16c458abc75d985828
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 22:06:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,9 @@ forst = 0
   while src.length > forst do
     seqond = 0 
     minimum_temp = []
+    lowest_temp_of_nested_array = -1
       while seqond < src[forst].count do
-        puts
+        if 
         minimum_temp << src[forst][seqond]
       end
       seqond += 1
@@ -14,17 +15,4 @@ forst = 0
       forst += 1
   end
  minimum_temp
-end
-
-  row_index = 0
-  while row_index < src.count do
-    element_index = 0
-    while element_index < src[row_index].count do
-      if src[row_index][element_index].even?
-        p src[row_index][element_index]
-      end
-      element_index += 1
-    end
-    row_index += 1
-  end
 end
