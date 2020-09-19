47a87619c37a88ee3c4bfaf8d5e7c161869084d0
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 22:32:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ while count < array_of_arrays.length do
  
   inner_count = 0
   while inner_count < array_of_arrays[count].length do
-    minimums = array_of_arrays[count][inner_count] 
+    minimums = array_of_arrays[count][inner_count].min 
      results_array << minimums
     inner_count += 1
   end
