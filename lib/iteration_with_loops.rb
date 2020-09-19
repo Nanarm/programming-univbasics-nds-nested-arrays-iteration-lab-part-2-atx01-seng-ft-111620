41c1135ec4ba9d95bd3772f3fb338927e33105d2
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 21:56:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,11 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+forst = 0 
+  while xxxx do
+    seqond = 
+      while yyyy do
+        puts
+      end
+  end
 end
