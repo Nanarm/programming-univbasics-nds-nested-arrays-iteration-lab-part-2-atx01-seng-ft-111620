b12031565295060b58c242b7dfe5a19480939b1e
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 22:02:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,11 +5,24 @@ forst = 0
   while src.length > forst do
     seqond = 0 
     minimum_temp = []
-      while  do
+      while seqond < src[forst].count do
         puts
       seqond += 1
       end
       forst += 1
   end
  minimum_temp
+end
+
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
 end
